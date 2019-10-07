bddafdf3a9d389a28b71ea83ccad324756482787
Piotr Antosz <piotr.antosz@insoh.pl>
2019-10-07 11:40:16 +0200

    change taxes to match our case

--- django_prices_vatlayer/management/commands/get_vat_rates.py
+++ django_prices_vatlayer/management/commands/get_vat_rates.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 from django.core.management.base import BaseCommand
 
 from ... import utils
@@ -7,8 +10,16 @@ class Command(BaseCommand):
     help = 'Get current vat rates in european country and saves to database'
 
     def handle(self, *args, **options):
-        json_response_rates = utils.fetch_vat_rates()
+        commands_dir = os.path.dirname(__file__)
+        rates_list_path = os.path.join(commands_dir, "rates_list.json")
+        types_path = os.path.join(commands_dir, "types.json")
+
+        with open(rates_list_path, "r") as read_file:
+            json_response_rates = json.load(read_file)
+
         utils.create_objects_from_json(json_response_rates)
 
-        json_response_types = utils.fetch_rate_types()
+        with open(types_path, "r") as read_file:
+            json_response_types = json.load(read_file)
+
         utils.save_vat_rate_types(json_response_types)
